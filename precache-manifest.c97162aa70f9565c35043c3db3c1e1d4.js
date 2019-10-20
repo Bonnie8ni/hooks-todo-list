91050a71561b446d66978b8f2b098edf8eb20ffa
BonnieYu <bonnieyu53@gmail.com>
2019-10-20 14:16:52 +0800

    Updates

---
+++ precache-manifest.c97162aa70f9565c35043c3db3c1e1d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26b4971323262049d6def59e3bfe89c",
+    "url": "/react-hooks-todo-list/index.html"
+  },
+  {
+    "revision": "06968cb30803d08c29f4",
+    "url": "/react-hooks-todo-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b6b33f49ed252e0744eb",
+    "url": "/react-hooks-todo-list/static/js/2.d83e9dc8.chunk.js"
+  },
+  {
+    "revision": "06968cb30803d08c29f4",
+    "url": "/react-hooks-todo-list/static/js/main.b148b615.chunk.js"
+  },
+  {
+    "revision": "376ee268ef3a6f9dffbc",
+    "url": "/react-hooks-todo-list/static/js/runtime-main.7be63587.js"
+  }
+]);
